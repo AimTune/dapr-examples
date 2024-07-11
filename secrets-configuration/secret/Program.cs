83fb027d781b01bfc64d65b8dea7e429cddb010a
= <=>
2024-07-11 06:09:04 +0300

    Configuration eklendi

--- secrets-configuration/secret/Program.cs
+++ secrets-configuration/secret/Program.cs
@@ -1,18 +1,25 @@
 using Dapr.Client;
 using Microsoft.AspNetCore.Mvc;
+using Dapr.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // builder.Services.AddDaprClient();
 
-builder.Services.AddSingleton<DaprClient>(instance =>
-{
-    return new DaprClientBuilder()
+DaprClient daprClient = new DaprClientBuilder()
         // .UseHttpEndpoint("http://localhost:3500/")
         .UseGrpcEndpoint("http://localhost:50001/")
         .UseJsonSerializationOptions(default)
         .Build();
+
+builder.Services.AddSingleton<DaprClient>(instance =>
+{
+    return daprClient;
 });
+
+
+builder.Configuration.AddDaprSecretStore("vault", daprClient, TimeSpan.FromSeconds(20));
+// builder.Configuration.AddDaprConfigurationStore("configstore", [], daprClient, TimeSpan.FromSeconds(60));
 
 var app = builder.Build();
 
@@ -24,6 +31,20 @@ app.MapGet("/get-secret", async ([FromServices] DaprClient daprClient) =>
 {
     var secret = await daprClient.GetSecretAsync("vault", "mysecret");
     return Results.Ok(secret);
+});
+
+app.MapGet("/get-configuration", async ([FromServices] IConfiguration configuration) =>
+{
+    var newConfiguration = await daprClient.GetConfiguration("configstore", new List<string>() { "orderId1", "orderId2" });
+
+    return Results.Ok(new
+    {
+        FirstKey = configuration["firstKey"],
+        SecondKey = configuration["secondKey"],
+        ThirdKey = configuration["thirdKey"],
+        OrderId1 = newConfiguration.Items["orderId1"].Value,
+        OrderId2 = newConfiguration.Items["orderId2"].Value
+    });
 });
 
 app.MapGet("/healthz", () => Results.Ok("OK"));
